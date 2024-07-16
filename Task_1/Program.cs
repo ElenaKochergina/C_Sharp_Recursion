3bacfc5a81d6dc22ac022d79b6ad3923bbb6ce8a
Alena <diva_a@mail.ru>
2024-07-16 16:16:13 +0300

    Add Task_1 in DZ_Seminar_7

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+   void Main()
+{
+    int M = ReadInt("Введите число M из отрезка [ M; N]: ");
+    int N = ReadInt("Введите число N из отрезка [ M; N]: ");
+    PrintNumbers(M, N); 
+}
+    static void PrintNumbers(int M, int N)
+    {
+        if (M > N) //рекурсия завершится, если выполнится условие
+        {
+            return;
+        }
+           Console.WriteLine(M);
+
+        PrintNumbers(M + 1, N); //рекурсивный вывод для следующего значения
+    }
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+Main();
